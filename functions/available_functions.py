526502681ab1004a4c00c91ce7c9dd44e5eda551
3xter666 <23520348@gm.uit.edu.vn>
2025-07-19 15:00:06 +0000

    Fixing minor errors

--- functions/available_functions.py
+++ functions/available_functions.py
@@ -1,10 +1,13 @@
-import functions
+from functions.get_file_content import schema_get_file_content
+from functions.get_files_info import schema_get_files_info
+from functions.run_python_file import schema_run_python_file
+from functions.write_file import schema_write_file
 from google.genai import types
 available_functions = types.Tool(
     function_declarations=[
-        functions.schema_get_files_info,
-        functions.schema_get_file_content,
-        functions.schema_run_python_file,
-        functions.schema_write_file,
+        schema_get_files_info,
+        schema_get_file_content,
+        schema_run_python_file,
+        schema_write_file,
     ]
 )
