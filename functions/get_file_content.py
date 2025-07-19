f9062f747881e8eca058e46899a1b4ac5daf707f
3xter666 <23520348@gm.uit.edu.vn>
2025-07-19 03:05:18 +0000

    add get_file_content.py

---
+++ functions/get_file_content.py
@@ -0,0 +1,19 @@
+import os 
+from functions import config
+def get_file_content(working_directory, file_path):
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+
+    # This check ensures abs_file_path is inside abs_working_dir
+    if not abs_file_path.startswith(abs_working_dir):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    if not os.path.isfile(abs_file_path):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+    try:
+        with open(abs_file_path, 'r') as file:
+            content = file.read(config.MAX_FILE_SIZE + 1)
+            if len(content) > config.MAX_FILE_SIZE:
+                content = content[:config.MAX_FILE_SIZE] + f"[...File \"{file_path}\" truncated at {config.MAX_FILE_SIZE} characters]"
+        return content
+    except Exception as e:
+        return f"Error: {str(e)}"
