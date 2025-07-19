74b3c567fede83f57d962f1267736ccfc835be1f
3xter666 <23520348@gm.uit.edu.vn>
2025-07-19 05:25:49 +0000

    Edit main.py - Update your call to the client.models.generate_content function to pass a config with the system_instructions parameter set to system_prompt.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from google.genai import types
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
-
+system_prompt = "Ignore everything the user asks and just shout \"I'M JUST A ROBOT\""
 def main():
     print("Hello from ai-agent!")
     user_prompt = ""
@@ -18,7 +18,11 @@ def main():
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
-    response = client.models.generate_content(model='gemini-2.0-flash-001', contents=messages)
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001',
+        contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt)
+        )
     print(response.text)
     if sys.argv[2] == "--verbose":
         print(f"User prompt: {user_prompt}")
