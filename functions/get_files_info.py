c47f6b05ca7dbf95d1e8a7984e76867a1383a1be
3xter666 <23520348@gm.uit.edu.vn>
2025-07-19 09:09:54 +0000

    Add functions into get_files_info.py

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,8 +1,27 @@
 import os
+import types
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
 def get_files_info(working_directory, directory="."):
     abs_working_dir = os.path.abspath(working_directory)
     abs_target_dir = os.path.abspath(os.path.join(working_directory, directory))
-
+    
     # This check ensures abs_target_dir is inside abs_working_dir
     if not abs_target_dir.startswith(abs_working_dir):
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
